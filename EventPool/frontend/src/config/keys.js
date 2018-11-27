a2f30867c7dedab085725bf2964c2689d1ddbdc3
Zohar Bergman <zohar.bergman@gmail.com>
2018-11-27 20:01:28 +0200

    Remove the api key to ignored file

---
+++ EventPool/frontend/src/config/keys.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Zohar on 27/11/2018.
+ */
+export const GOOGLE_GEOCODE_API_KEY = '';
--- EventPool/frontend/src/util/Geocoding.js
+++ EventPool/frontend/src/util/Geocoding.js
@@ -2,11 +2,12 @@
  * Created by Zohar on 20/11/2018.
  */
 import { createClient } from '@google/maps';
+import { GOOGLE_GEOCODE_API_KEY } from '../config/keys';
 
 let googleMapsClient;
 
 function initialize() {
-    googleMapsClient = createClient({key: ''});
+    googleMapsClient = createClient({key: GOOGLE_GEOCODE_API_KEY});
 }
 
 function getGoogleMapsClient() {
