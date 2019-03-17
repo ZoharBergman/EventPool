b85ef0c9617f39515ed840038e353f43eba58355
Zohar Bergman <zohar.bergman@gmail.com>
2019-03-17 15:06:21 +0200

    Make the potential match calculator into a threads' pool. Not finished yet.

--- EventPool/src/main/java/com/FinalProject/EventPool/BL/CarpoolMatching/CarpoolMatchingBL.java
+++ EventPool/src/main/java/com/FinalProject/EventPool/BL/CarpoolMatching/CarpoolMatchingBL.java
@@ -15,6 +15,8 @@ import org.jgrapht.graph.DirectedWeightedMultigraph;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Semaphore;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -42,104 +44,89 @@ public class CarpoolMatchingBL implements ICarpoolMatching {
 
     private Table<Driver, Passenger, Double> calcPotentialMatching(Double deviationRadius, List<Passenger> lstPassengers, String eventId) {
         Table<Driver, Passenger, Double> potentialMatches = HashBasedTable.create();
-        Map<String, Driver> mapDriversById = new HashMap<>();
+        ConcurrentMap<String, Driver> mapDriversById = new ConcurrentHashMap<>();
 
         // Going over the passengers
         lstPassengers.forEach(passenger -> {
-            final Semaphore semaphore = new Semaphore(0);
-
-            // Find the locations of the drivers that are driving near the current passenger
-            Geofire.getInstance(eventId).queryAtLocation(new GeoLocation(passenger.getStartLocation().lat, passenger.getStartLocation().lng),
-                            deviationRadius)
-                    .addGeoQueryDataEventListener(new GeoQueryDataEventListener() {
-                        @Override
-                        public void onDataEntered(DataSnapshot locationSnapshot, GeoLocation geoLocation) {
-                            // Get the drivers that are driving through the location
-                            GeofireToDriver.getReference().child(eventId).child(locationSnapshot.getKey())
-                                    .addListenerForSingleValueEvent(new ValueEventListener() {
-                                        @Override
-                                        public void onDataChange(DataSnapshot driversSnapshot) {
-                                            // Going over the drivers
-                                            ((Map)driversSnapshot.getValue()).forEach((driverId, freeSeatsNum) -> {
-                                                Driver driver;
-                                                if (mapDriversById.containsKey(driverId)) {
-                                                    driver = mapDriversById.get(driverId);
-                                                } else {
-                                                    driver = new Driver(driverId.toString(), new Integer(freeSeatsNum.toString()));
-                                                    mapDriversById.put(driver.getDriverId(), driver);
-                                                }
-
-                                                Double distance = distanceInKm(
-                                                        passenger.getStartLocation().lat,
-                                                        passenger.getStartLocation().lng,
-                                                        geoLocation.latitude,
-                                                        geoLocation.longitude);
-
-                                                if (potentialMatches.contains(driver, passenger)) {
-                                                    potentialMatches.put(
-                                                            driver,
-                                                            passenger,
-                                                            Math.min(distance, potentialMatches.get(driver, passenger)));
-                                                } else {
-                                                    potentialMatches.put(driver, passenger, distance);
-                                                }
-                                            });
-                                        }
-
-                                        @Override
-                                        public void onCancelled(DatabaseError databaseError) {
-
-                                        }
-                                    });
-                        }
-
-                        @Override
-                        public void onDataExited(DataSnapshot dataSnapshot) {
-                        }
-
-                        @Override
-                        public void onDataMoved(DataSnapshot dataSnapshot, GeoLocation geoLocation) {
-                        }
-
-                        @Override
-                        public void onDataChanged(DataSnapshot dataSnapshot, GeoLocation geoLocation) {
-                        }
-
-                        @Override
-                        public void onGeoQueryReady() {
-                            semaphore.release();
-                        }
-
-                        @Override
-                        public void onGeoQueryError(DatabaseError databaseError) {
-                        }
-                    });
-
-            try {
-                semaphore.acquire();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+//            final Semaphore semaphore = new Semaphore(0);
+//
+//            // Find the locations of the drivers that are driving near the current passenger
+//            Geofire.getInstance(eventId).queryAtLocation(new GeoLocation(passenger.getStartLocation().lat, passenger.getStartLocation().lng),
+//                    deviationRadius)
+//                    .addGeoQueryDataEventListener(new GeoQueryDataEventListener() {
+//                        @Override
+//                        public void onDataEntered(DataSnapshot locationSnapshot, GeoLocation geoLocation) {
+//                            // Get the drivers that are driving through the location
+//                            GeofireToDriver.getReference().child(eventId).child(locationSnapshot.getKey())
+//                                    .addListenerForSingleValueEvent(new ValueEventListener() {
+//                                        @Override
+//                                        public void onDataChange(DataSnapshot driversSnapshot) {
+//                                            // Going over the drivers
+//                                            ((Map)driversSnapshot.getValue()).forEach((driverId, freeSeatsNum) -> {
+//                                                Driver driver;
+//                                                if (mapDriversById.containsKey(driverId)) {
+//                                                    driver = mapDriversById.get(driverId);
+//                                                } else {
+//                                                    driver = new Driver(driverId.toString(), new Integer(freeSeatsNum.toString()));
+//                                                    mapDriversById.put(driver.getDriverId(), driver);
+//                                                }
+//
+//                                                Double distance = distanceInKm(
+//                                                        passenger.getStartLocation().lat,
+//                                                        passenger.getStartLocation().lng,
+//                                                        geoLocation.latitude,
+//                                                        geoLocation.longitude);
+//
+//                                                synchronized (potentialMatches) {
+//                                                    if (potentialMatches.contains(driver, passenger)) {
+//                                                        potentialMatches.put(
+//                                                                driver,
+//                                                                passenger,
+//                                                                Math.min(distance, potentialMatches.get(driver, passenger)));
+//                                                    } else {
+//                                                        potentialMatches.put(driver, passenger, distance);
+//                                                    }
+//                                                }
+//                                            });
+//                                        }
+//
+//                                        @Override
+//                                        public void onCancelled(DatabaseError databaseError) {
+//
+//                                        }
+//                                    });
+//                        }
+//
+//                        @Override
+//                        public void onDataExited(DataSnapshot dataSnapshot) {
+//                        }
+//
+//                        @Override
+//                        public void onDataMoved(DataSnapshot dataSnapshot, GeoLocation geoLocation) {
+//                        }
+//
+//                        @Override
+//                        public void onDataChanged(DataSnapshot dataSnapshot, GeoLocation geoLocation) {
+//                        }
+//
+//                        @Override
+//                        public void onGeoQueryReady() {
+//                            semaphore.release();
+//                        }
+//
+//                        @Override
+//                        public void onGeoQueryError(DatabaseError databaseError) {
+//                        }
+//                    });
+//
+//            try {
+//                semaphore.acquire();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
         });
 
         return potentialMatches;
-    }
-
-    private static double distanceInKm(double lat1, double lon1, double lat2, double lon2) {
-        if ((lat1 == lat2) && (lon1 == lon2)) {
-            return 0;
-        }
-        else {
-            double theta = lon1 - lon2;
-            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
-            dist = Math.acos(dist);
-            dist = Math.toDegrees(dist);
-            dist = dist * 60 * 1.1515;
-            // Make the dist in KM
-            dist = dist * 1.609344;
-
-            return (dist);
-        }
     }
 
     private List<Passenger> getPassengers(String eventId) throws InterruptedException {
---
+++ EventPool/src/main/java/com/FinalProject/EventPool/BL/CarpoolMatching/PotentialMatchThread.java
@@ -0,0 +1,156 @@
+package com.FinalProject.EventPool.BL.CarpoolMatching;
+
+import com.FinalProject.EventPool.Models.Driver;
+import com.FinalProject.EventPool.Models.Geofire;
+import com.FinalProject.EventPool.Models.GeofireToDriver;
+import com.FinalProject.EventPool.Models.Passenger;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQueryDataEventListener;
+import com.google.common.collect.Table;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by Zohar on 17/03/2019.
+ */
+public class PotentialMatchThread extends Thread{
+    // Properties
+    final Table<Driver, Passenger, Double> potentialMatches;
+    Double deviationRadius;
+    Passenger passenger;
+    String eventId;
+    Integer numOfHandledGeoLocations;
+    ConcurrentMap<String, Driver> mapDriversById;
+
+    // Ctor
+    public PotentialMatchThread(Table<Driver, Passenger, Double> potentialMatches, Double deviationRadius, Passenger passenger, String eventId, ConcurrentMap<String, Driver> mapDriversById) {
+        super();
+        this.potentialMatches = potentialMatches;
+        this.deviationRadius = deviationRadius;
+        this.passenger = passenger;
+        this.eventId = eventId;
+        this.mapDriversById = mapDriversById;
+        this.numOfHandledGeoLocations = 0;
+    }
+
+    @Override
+    public void run() {
+        Semaphore geoLocationQuery = new Semaphore(0);
+        Semaphore counterIsZero = new Semaphore(0);
+
+        // Find the locations of the drivers that are driving near the passenger
+        Geofire.getInstance(eventId).queryAtLocation(new GeoLocation(passenger.getStartLocation().lat, passenger.getStartLocation().lng),
+                deviationRadius)
+                .addGeoQueryDataEventListener(new GeoQueryDataEventListener() {
+                    @Override
+                    public void onDataEntered(DataSnapshot locationSnapshot, GeoLocation geoLocation) {
+                        synchronized (numOfHandledGeoLocations) {
+                            numOfHandledGeoLocations++;
+                        }
+
+                        // Get the drivers that are driving through the location
+                        GeofireToDriver.getReference().child(eventId).child(locationSnapshot.getKey())
+                                .addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot driversSnapshot) {
+                                        // Going over the drivers
+                                        ((Map)driversSnapshot.getValue()).forEach((driverId, freeSeatsNum) -> {
+                                            Driver driver;
+                                            synchronized (mapDriversById) {
+                                                if (mapDriversById.containsKey(driverId)) {
+                                                    driver = mapDriversById.get(driverId);
+                                                } else {
+                                                    driver = new Driver(driverId.toString(), new Integer(freeSeatsNum.toString()));
+                                                    mapDriversById.put(driver.getDriverId(), driver);
+                                                }
+                                            }
+
+                                            Double distance = distanceInKm(
+                                                    passenger.getStartLocation().lat,
+                                                    passenger.getStartLocation().lng,
+                                                    geoLocation.latitude,
+                                                    geoLocation.longitude);
+
+                                            synchronized (potentialMatches) {
+                                                if (potentialMatches.contains(driver, passenger)) {
+                                                    potentialMatches.put(
+                                                            driver,
+                                                            passenger,
+                                                            Math.min(distance, potentialMatches.get(driver, passenger)));
+                                                } else {
+                                                    potentialMatches.put(driver, passenger, distance);
+                                                }
+                                            }
+                                        });
+
+                                        synchronized (numOfHandledGeoLocations) {
+                                            numOfHandledGeoLocations--;
+
+                                            if (numOfHandledGeoLocations == 0) {
+                                                counterIsZero.release();
+                                            }
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onDataExited(DataSnapshot dataSnapshot) {
+                    }
+
+                    @Override
+                    public void onDataMoved(DataSnapshot dataSnapshot, GeoLocation geoLocation) {
+                    }
+
+                    @Override
+                    public void onDataChanged(DataSnapshot dataSnapshot, GeoLocation geoLocation) {
+                    }
+
+                    @Override
+                    public void onGeoQueryReady() {
+                        geoLocationQuery.release();
+                    }
+
+                    @Override
+                    public void onGeoQueryError(DatabaseError databaseError) {
+                    }
+                });
+
+        try {
+            geoLocationQuery.acquire();
+
+            while (numOfHandledGeoLocations > 0) {
+                counterIsZero.acquire();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static double distanceInKm(double lat1, double lon1, double lat2, double lon2) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        }
+        else {
+            double theta = lon1 - lon2;
+            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+            // Make the dist in KM
+            dist = dist * 1.609344;
+
+            return (dist);
+        }
+    }
+}
